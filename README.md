b3e2291cb7e289393942e04d8d2b385fa98fd92e
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-04 02:16:58 +0300

    Donuts-Factory

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Donuts-Factory
+# Dount Shop Website
+Welcome to our Dount Shop! 🍰
+To provide you with a personalized experience, please share your name and gender
---
+++ app.js
@@ -0,0 +1,30 @@
+let name = prompt('Please enter your name');
+
+let gender = prompt('Please enter your gender');
+if (gender == 'femail')
+    {
+        alert('Welcom Ms');
+    }
+else if(gender=='mail')
+    {
+        alert('Welcom Mr');
+    }
+else
+    {
+        alert('Welcom');
+    }
+
+let order = prompt('Would you like to order a donut, coffee, ice cream, or bakery items?');
+
+let orderName = prompt('Please write down the name of your order')
+
+alert ('Your order is being prepared')
+
+console.log(name, " " ,orderName)
+document.write(name, " " ,orderName)
+
+
+
+
+
+
