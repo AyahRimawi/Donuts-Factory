ee0433ed42a4980fb416ae8365b14b292c73b0cf
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-04 22:57:31 +0300

    Donuts-Factory

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-let name = prompt('Please enter your name');
+let username = prompt('Please enter your name');
+
+// let gender;
+// do {
+//     gender = prompt('Please enter your gender');
+// } while (gender !== 'female' && gender !== 'male');
+
+// if (gender == 'female') {
+//     alert('Welcome Ms');
+// } else {
+//     alert('Welcome Mr');
+// }
+
 
 let gender = prompt('Please enter your gender');
-if (gender == 'femail')
+while (gender!=='female'&& gender!=='male'){
+    gender = prompt('Please enter your gender');
+}
+if (gender == 'female')
     {
         alert('Welcom Ms');
     }
-else if(gender=='mail')
+else
     {
         alert('Welcom Mr');
     }
-else
-    {
-        alert('Welcom');
-    }
+
+
+
 
 let order = prompt('Would you like to order a donut, coffee, ice cream, or bakery items?');
 
@@ -20,11 +34,18 @@ let orderName = prompt('Please write down the name of your order')
 
 alert ('Your order is being prepared')
 
-console.log(name, " " ,orderName)
-document.write(name, " " ,orderName)
+console.log(username, " " ,orderName)
+document.write(username, " " ,orderName)
 
+Array = [username, gender, order, orderName];
+console.log(Array);
+console.log("Username:", Array[0]);
+console.log("Gender:", Array[1]);
+console.log("Order:", Array[2]);
+console.log("Order Name:", Array[3]);
 
-
+for (let i = 0; i < Array.length; i++) {
+    console.log(Array[i]);}
 
 
 
