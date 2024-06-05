56b0276788ae8b7084a0075c9ec755792b5e1b37
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-05 22:43:35 +0300

    Donuts-Factory

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ let username = prompt('Please enter your name');
 // }
 
 
-let gender = prompt('Please enter your gender');
-while (gender!=='female'&& gender!=='male'){
-    gender = prompt('Please enter your gender');
-}
-if (gender == 'female')
-    {
-        alert('Welcom Ms');
-    }
-else
-    {
-        alert('Welcom Mr');
-    }
+// let gender = prompt('Please enter your gender');
+// while (gender!=='female'&& gender!=='male'){
+//     gender = prompt('Please enter your gender');
+// }
+// if (gender == 'female')
+//     {
+//         alert('Welcom Ms');
+//     }
+// else
+//     {
+//         alert('Welcom Mr');
+//     }
 
 
 
